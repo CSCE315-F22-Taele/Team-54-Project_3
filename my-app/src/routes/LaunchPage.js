52804cb6998025a821a9b384ab001c89390c4a18
mochiflootloops <m.ghosh.3522@gmail.com>
2022-11-05 17:20:06 -0500

    launchpage buttons navigate successfully, need to check formatting in some of the pages

--- my-app/src/routes/LaunchPage.js
+++ my-app/src/routes/LaunchPage.js
@@ -1,11 +1,22 @@
 import React from "react";
+import {useNavigate} from "react-router-dom";
 
 const LaunchPage = () => {
+    let navigate = useNavigate()
+
+    const handleUpdate = (page) => {
+        navigate(`/${page}`);
+    };
+
     return (
         <div>
             <h1>
                 Launch Page
             </h1>
+
+            <button onClick={() => handleUpdate("Cashier")}>Cashier</button>
+            <button onClick={() => handleUpdate("Customer")}>Customer</button>
+            <button onClick={() => handleUpdate("Manager")}>Manager</button>
         </div>
 
     )
