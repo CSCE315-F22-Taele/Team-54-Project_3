a7a16a9eeb9169ff74f026b3684c07f737ba6d80
neha2003 <neha.sq8@gmail.com>
2022-11-05 20:22:56 -0500

    added categories manually

--- my-app/src/routes/Cashier.js
+++ my-app/src/routes/Cashier.js
@@ -1,54 +1,11 @@
-// import React, {useState} from "react";
-// import Menu from "./Menu";
-// import Categories from "./Categories";
-// import items from "./data";
-
-// const allCategories = ["all", ...new Set(items.map((item) => item.category))];
-
-// const Cashier = () => {
-//     const [menuItems, setMenuItems] = useState(items);
-//     const [activeCategory, setActiveCategory] = useState("");
-//     const [categories, setCategories] = useState(allCategories);
-
-//     const filterItems = (category) => {
-//         setActiveCategory(category);
-
-//         if (category === "all") {
-//             setMenuItems(items);
-//             return;
-//         }
-
-//         const newItems = items.filter((item) => item.category === category);
-//         setMenuItems(newItems);
-//     };
-//     return (
-//         // <div>
-//         //     <h1 className="font-weight-light display-1 text-center">
-//         //         Cashier
-//         //     </h1>
-//         // </div>
-//         <main>
-//             <section className = "menu section">
-//                 <div className = "title">
-//                     <h2>Menu List</h2>
-                    
-//                 </div>
-//                 {/* <Categories catgories={categories} activeCategory={activeCategory} filterItems={filterItems} /> */}
-                
-//             </section>
-//         </main>
-//     );
-// };
-
-// export default Cashier;
-
 import React, { useState } from "react";
 import Menu from "./Menu";
 import Categories from "./Categories";
 import items from "./data";
 // import logo from "./logo.JPG";
 
-const allCategories = ["all", ...new Set(items.map((item) => item.category))];
+const allCategories = ["Breakfast", "Entree", "Salads", "Sides", "Kids Meals", "Treats", "Drinks", "Sauce"];
+console.log(allCategories)
 
 const Cashier = () => {
   const [menuItems, setMenuItems] = useState(items);
