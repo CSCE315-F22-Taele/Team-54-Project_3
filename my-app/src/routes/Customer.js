3f7ce32a4521f45896e4a52df779fbba7445e42e
mochiflootloops <m.ghosh.3522@gmail.com>
2022-11-05 23:23:01 -0500

    Duplicated Cashier code in Customer

--- my-app/src/routes/Customer.js
+++ my-app/src/routes/Customer.js
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
+// import logo from "./logo.JPG";
+
+const allCategories = ["Breakfast", "Entree", "Salads", "Sides", "Kids Meals", "Treats", "Drinks", "Sauce"];
 
 const Customer = () => {
-    return (
-        <div>
-            <h1 className="font-weight-light display-1 text-center">
-                Customer
-            </h1>
-        </div>
+  const [menuItems, setMenuItems] = useState(items);
+  const [activeCategory, setActiveCategory] = useState("");
+  const [categories, setCategories] = useState(allCategories);
 
-    )
-}
+  const filterItems = (category) => {
+    setActiveCategory(category);
+    if (category === "all") {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          {/* <img src={logo} alt="logo" className="logo" /> */}
+          <h2 style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>Menu List</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories
+          categories={categories}
+          activeCategory={activeCategory}
+          filterItems={filterItems}
+        />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
+};
 
 export default Customer;
