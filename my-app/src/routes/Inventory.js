5c63c832279a1723430cafca9ed06d21e8e423ba
neha2003 <neha.sq8@gmail.com>
2022-11-07 23:22:28 -0600

    made nav bar persistent on inventory and MenuEditor

--- my-app/src/routes/Inventory.js
+++ my-app/src/routes/Inventory.js
@@ -1,10 +1,36 @@
 import React from "react";
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import {useNavigate} from "react-router-dom";
 
 const Inventory = () => {
+    let navigate = useNavigate()
+
+    const handleUpdate = (page) => {
+        navigate(`/${page}`);
+    };
     return (
-        <div>
-            <h1 className="font-weight-light display-1 text-center">Inventory</h1>
-        </div>
+        <Navbar bg="dark" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand>Manager</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link style={{color:"red"}} onClick={() => handleUpdate("Inventory")}>Inventory</Nav.Link>
+            <Nav.Link onClick={() => handleUpdate("MenuEditor")}>MenuEditor</Nav.Link>
+            <NavDropdown title="Order Trends" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Sales Report</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Excess Report
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Restock Report</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     )
 }
 
