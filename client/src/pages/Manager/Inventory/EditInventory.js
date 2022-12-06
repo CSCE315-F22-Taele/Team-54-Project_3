11cd8314e8b722f066d5dd959f3a67bbb1d7a8d0
neha2003 <neha.sq8@gmail.com>
2022-12-06 03:53:47 -0600

    fixed EditInventory

--- client/src/pages/Manager/Inventory/EditInventory.js
+++ client/src/pages/Manager/Inventory/EditInventory.js
@@ -32,6 +32,18 @@ const EditInventory = () => {
         e.preventDefault();
     }
     const onInput = () => {
+
+    }
+    const handleAdd = () => {
+
+    }
+    const handleEdit = () => {
+
+    }
+    const handleDelete = () => {
+        
+    }
+    const handleSubmit = () => {
         
     }
 
@@ -41,16 +53,67 @@ const EditInventory = () => {
                 <Form.Group>
                     <Form.Control 
                     type="text" 
-                    placeholder="Enter quantity" 
+                    placeholder="Enter item ID" 
                     onChange={onInput}
                     />
-                    {/* <button 
-                        onClick={handleOrders}
+                    <Form.Control 
+                    type="text" 
+                    placeholder="Enter item name" 
+                    onChange={onInput}
+                    />
+                    <Form.Control 
+                    type="text" 
+                    placeholder="Enter category" 
+                    onChange={onInput}
+                    />
+                    <Form.Control 
+                    type="text" 
+                    placeholder="Enter expiration date" 
+                    onChange={onInput}
+                    />
+                    <Form.Control 
+                    type="text" 
+                    placeholder="Enter refrigeration requirement" 
+                    onChange={onInput}
+                    />
+                    <Form.Control 
+                    type="text" 
+                    placeholder="Enter quantity in stock" 
+                    onChange={onInput}
+                    />
+                    <Form.Control 
+                    type="text" 
+                    placeholder="Enter unit" 
+                    onChange={onInput}
+                    />
+                    <button 
+                        onClick={handleAdd}
+                        className="btn btn-success " 
+                        style={{alignSelf: 'center', justifyContent: 'center'}} 
+                        type="submit">
+                        Add item
+                    </button>
+                    <button 
+                        onClick={handleEdit}
+                        className="btn btn-secondary " 
+                        style={{alignSelf: 'center', justifyContent: 'center'}} 
+                        type="submit">
+                        Edit item
+                    </button>
+                    <button 
+                        onClick={handleDelete}
                         className="btn btn-danger " 
                         style={{alignSelf: 'center', justifyContent: 'center'}} 
                         type="submit">
-                        Order
-                    </button> */}
+                        Delete item
+                    </button>
+                    <button 
+                        onClick={handleSubmit}
+                        className="btn btn-primary " 
+                        style={{alignSelf: 'center', justifyContent: 'center'}} 
+                        type="submit">
+                        Submit
+                    </button>
                 </Form.Group>
             </Form>
         </div>
