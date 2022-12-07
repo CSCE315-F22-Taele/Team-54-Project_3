0e6457f68fca08577319dca8e7ffad7425a0bdaa
escchen <estellaschen@gmail.com>
2022-12-06 20:20:42 -0600

    implemented oauth

--- client/src/pages/MainPage/LaunchPage.js
+++ client/src/pages/MainPage/LaunchPage.js
@@ -5,9 +5,10 @@
  * @author Mohona Ghosh
  */
 import React from "react";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import {useNavigate} from "react-router-dom";
 import logo from "./chicken-logo.jpg";
+import jwt_decode from "jwt-decode";
 
 /**
  * Constructs and returns a LaunchPage object displaying team logo, Google Translate component, and buttons to access each user.
@@ -15,6 +16,8 @@ import logo from "./chicken-logo.jpg";
  */
 const LaunchPage = () => {
     let navigate = useNavigate()
+
+    const [user, setUser] = useState({});
 
     /**
      * Allows the user to navigate to a certain page depending on the button clicked.
@@ -28,29 +31,77 @@ const LaunchPage = () => {
      * Initalizes the Google Translate element that shows up on the LaunchPage.
      */
     const googleTranslateElementInit = () => {
-        new window.google.translate.TranslateElement(
-          {
-            pageLanguage: "en",
-            autoDisplay: false
-          },
-          "google_translate_element"
-        );
-      };
+      new window.google.translate.TranslateElement(
+        {
+          pageLanguage: "en",
+          autoDisplay: false
+        },
+        "google_translate_element"
+      );
+    };
 
-      useEffect(() => {
-        var addScript = document.createElement("script");
-        addScript.setAttribute(
-          "src",
-          "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
-        );
-        document.body.appendChild(addScript);
-        window.googleTranslateElementInit = googleTranslateElementInit;
-      }, []);
+    /**
+     * 
+     */
+    function handleCallbackResponse(response) {
+      console.log("Encoded JWT ID token: " + response.credential);
+      var userObject = jwt_decode(response.credential);
+      console.log(userObject);
+      setUser(userObject);
+
+      document.getElementById("signInDiv").hidden = true;
+    }
+
+    /**
+     * 
+     */
+    function handleSignOut(event) {
+      setUser({});
+      document.getElementById("signInDiv").hidden = false;
+    }
+
+    useEffect(() => {
+      var addScript = document.createElement("script");
+      addScript.setAttribute(
+        "src",
+        "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
+      );
+      document.body.appendChild(addScript);
+      window.googleTranslateElementInit = googleTranslateElementInit;
+
+      /* global google */
+      google.accounts.id.initialize({
+        client_id: "971843556198-g2dablttraii3memss2rmmfeqp4pv4dn.apps.googleusercontent.com",
+        callback: handleCallbackResponse
+      });
+
+      google.accounts.id.renderButton(
+        document.getElementById("signInDiv"),
+        { theme: "outline", size: "large" }
+      );
+
+      google.accounts.id.prompt();
+    }, []);
+
+    // if we have no user: sign in button
+    // if we have a user: show the log out button
 
     return (
       <div>
           <div>
             <div id="google_translate_element" /*style="text-align: 'right'; vertical-align: text-top;"*/></div>
+          </div>
+          <div>
+            <div id="signInDiv"></div>
+            { Object.keys(user).length != 0 &&
+              <button onClick={ (e) => handleSignOut(e)}>Sign Out</button>
+            }
+            { user &&
+              <div>
+                <img src={user.picture}></img>
+                <h3>{user.name}</h3>
+              </div>
+            }
           </div>
           <img width="200" className="rounded mx-auto d-block" src={logo} alt="chicken-yurh"/>
           <h1 className="font-weight-light display-1 text-center">
